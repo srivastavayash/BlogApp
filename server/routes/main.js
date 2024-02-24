631edbfa29a6a10984e1a4a03f67a03747711ea4
Yash Srivastava <srivastavayash017@gmail.com>
2024-02-25 01:28:10 +0530

    changes

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,11 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(req,res)=>{
-    res.send("ALL OK👍");
+    res.render('index');
+});
+
+router.get('/about',(req,res)=>{
+    res.render('about');
 });
 
 module.exports=router;
