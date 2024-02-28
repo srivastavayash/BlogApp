958d8827b6bc5ad6fb108b425f3673ab6b065521
Yash Srivastava <srivastavayash017@gmail.com>
2024-02-28 22:11:53 +0530

    Pagination

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 require('dotenv').config();
 
-const express=require('express');
-const expressLayout=require('express-ejs-layouts');
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 
-const app=express();
-const PORT=5000 || process.env.PORT;
+const connectDB = require('./server/config/db');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+// connect DataBase
+connectDB();
 
 app.use(express.static('public'));
 
 //Template Engine
 app.use(expressLayout);
-app.set('layout','./layouts/main');
-app.set('view engine','ejs');
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-app.use('/',require('./server/routes/main'));
+app.use('/', require('./server/routes/main'));
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is running on Localhost:${PORT}`);
 });
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,119 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const { parse } = require('dotenv');
 
-router.get('/',(req,res)=>{
-    const locals={
-        title:"BlogApp",
-        decription:"Blogging application based NODEJS,EXPRESSJS and MONGODB"
+
+
+// Route to get/Home
+router.get('/', async (req, res) => {
+    try {
+        const locals = {
+            title: "BlogApp",
+            decription: "Blogging application based NODEJS,EXPRESSJS and MONGODB"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        const count = await Post.countDocuments(Post.title);
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        // const data = await Post.find();
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+    } catch (err) {
+        console.log(err);
     }
-    res.render('index',{locals});
+
 });
 
-router.get('/about',(req,res)=>{
+
+// router.get('/', async (req, res) => {
+//     const locals = {
+//         title: "BlogApp",
+//         decription: "Blogging application based NODEJS,EXPRESSJS and MONGODB"
+//     }
+//     try {
+//         const data = await Post.find();
+//         res.render('index', { locals, data });
+//     } catch (err) {
+//         console.log(err);
+//     }
+
+// });
+
+
+
+
+
+router.get('/about', (req, res) => {
     res.render('about');
 });
 
-router.get('/contact',(req,res)=>{
+router.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-module.exports=router;
+
+//FakeData
+
+// function insertPostData() {
+//     Post.insertMany([
+//         {
+//             title: "Building a Blog",
+//             body: "this is the body text"
+//         },
+//         {
+//             title: "Deployment of Node.js applications",
+//             body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
+//         },
+//         {
+//             title: "Authentication and Authorization in Node.js",
+//             body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
+//         },
+//         {
+//             title: "Understand how to work with MongoDB and Mongoose",
+//             body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
+//         },
+//         {
+//             title: "build real-time, event-driven applications in Node.js",
+//             body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+//         },
+//         {
+//             title: "Discover how to use Express.js",
+//             body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
+//         },
+//         {
+//             title: "Asynchronous Programming with Node.js",
+//             body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
+//         },
+//         {
+//             title: "Learn the basics of Node.js and its architecture",
+//             body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
+//         },
+//         {
+//             title: "NodeJs Limiting Network Traffic",
+//             body: "Learn how to limit netowrk traffic."
+//         },
+//         {
+//             title: "Learn Morgan - HTTP Request logger for NodeJs",
+//             body: "Learn Morgan."
+//         },
+//     ])
+// }
+// insertPostData();
+
+
+
+module.exports = router;
